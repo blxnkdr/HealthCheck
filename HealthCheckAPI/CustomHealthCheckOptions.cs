2d1e8ee61a1743774cf07c290aa58444a927152f
Des <Des@DESKTOP-JVLJT7K>
2023-10-19 08:56:30 -0400

    Add project files.

---
+++ HealthCheckAPI/CustomHealthCheckOptions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Xml.Schema;
+
+namespace HealthCheckAPI
+{
+    public class CustomHealthCheckOptions : HealthCheckOptions
+    {
+        public CustomHealthCheckOptions() : base()
+        {
+            var jsonSerializationOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            ResponseWriter = async (c, r) =>
+            {
+                c.Response.ContentType =
+                MediaTypeNames.Application.Json;
+                c.Response.StatusCode = StatusCodes.Status200OK;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    checks = r.Entries.Select(e => new
+                    {
+                        name = e.Key,
+
+                        responseTime =
+                        e.Value.Duration.TotalMilliseconds,
+                        status = e.Value.Status.ToString(),
+                        description = e.Value.Description
+                    }),
+                    totalStatus = r.Status,
+                    totalResponseTime =
+                    r.TotalDuration.TotalMilliseconds,
+                }, jsonSerializationOptions);
+                await c.Response.WriteAsync(result);
+            };
+        }
+
+    }
+}
